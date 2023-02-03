b36b09f6a66db98399a2cd0ccbc2014773299816
GusttaDev <gustta.dev@gmail.com>
2023-02-03 16:21:42 -0300

    Initial commit

---
+++ src/main/java/br/com/desafiodio/gof/LabDesingPatternApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafiodio.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabDesingPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabDesingPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafiodio/gof/LabDesingPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafiodio.gof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabDesingPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
